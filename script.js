80c03d01fa94e5e8f9529ee29ac299f2409d1909
nobleat <88677746+nobleat@users.noreply.github.com>
2021-10-10 10:48:07 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("page loaded")
+
+
+function popup(){
+    alert("Loading weather report...")
+}
+
+var accept = document.querySelector("#cookies")
+function acceptcookie(){
+    accept.remove()
+}
+
+//function temp(element){
+//    if (element.value == "F")
+//    var c1 = document.querySelector("c1");
+//    c1.innerText = "75&deg;";
+//    else
+//}
